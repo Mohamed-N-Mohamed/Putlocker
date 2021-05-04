63b88d5d5d722335ee738d67b03aa06db1e44248
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2021-05-04 23:29:39 +0100

    added movie component

---
+++ src/components/Movie.js
@@ -0,0 +1,21 @@
+import React from 'react';
+//api key
+const img_api = 'https://image.tmdb.org/t/p/w1280';
+
+//movies - title, poster_path, overview, vote_average
+
+const Movie = ({title, poster_path, overview, vote_average}) => (
+  <div className="movie">
+    <img src={img_api  + poster_path} alt={title}/>
+    <div className="movie-info">
+     <h3>{title}</h3>
+     <span>{vote_average}</span> 
+    </div>
+    
+
+  </div>
+)
+
+
+
+export default  Movie;
