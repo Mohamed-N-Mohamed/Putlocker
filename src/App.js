dcde881fe890dae3176d6a8ed7c1a2f65e8c2e8b
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2021-05-07 20:02:22 +0100

    added search for movie/tv functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import Movie from './components/Movie'
+
+
+
+
+//api keys 
+const features_api_key = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=ffac1da63b7872d8587fb0e12d9051b0';
+
+const search_api_key = 'https://api.themoviedb.org/3/search/movie?&api_key=ffac1da63b7872d8587fb0e12d9051b0&query=';
+
+const img_api = 'https://image.tmdb.org/t/p/w1280';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const  [movies, setMovies] = useState([]);
+ const [searchTerm, setSearchTerm] = useState('');
+
+ useEffect( () => {
+   //call api
+   async function fetchData() {
+    const moviesResponse = await fetch(features_api_key);
+   const moviesJson = await moviesResponse.json();
+     setMovies(moviesJson.results)
+  }
+
+  fetchData();
+  
+
+ }, []);
+
+ //searches movies when typed in search bar
+ const searchMovie = async (e) => {
+   e.preventDefault();
+  
+   const moviesResponse = await fetch(search_api_key+searchTerm);
+   const moviesJson = await moviesResponse.json();
+     setMovies(moviesJson.results)
+
+ }
+
+
+//when typed in search bar get the value
+ const handleOnChange =(e) => {
+   setSearchTerm(e.target.value)
+  
+
+ }
+
+
+ return (
+   
+
+  <div>
+
+<header>
+    <form onSubmit={searchMovie}>
+    <input type="text" placeholder="Search" className="search" value={searchTerm} onChange={handleOnChange}/>
+    </form>
+  </header>
+  
+  <div className ="movie-container">
+
+  {movies.map((movie) => (
+    <Movie key={movie.id} {...movie}/> 
+  ))}
+  
+</div>
+</div>
+ )
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -11,6 +11,22 @@ const Movie = ({title, poster_path, overview, vote_average}) => (
      <h3>{title}</h3>
      <span>{vote_average}</span> 
     </div>
+
+    
+    <div className="movie-overview">
+      <h2>Movie Info</h2>
+      <p>{overview}</p>
+
+    </div>
+
+
+    <div className="hd">
+        HD
+
+    </div>
+
+
+
     
 
   </div>
