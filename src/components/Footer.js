d7180224a9282981f0e3ada5123af056f6613501
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2021-05-09 20:26:00 +0100

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+
+const Footer = () => {
+    return (
+      <div className="footer">
+      <p>&copy; 2021 Mohamed Mohamed</p>
+       
+     </div>
+    )
+    
+
+}
+
+
+
+
+export default Footer
